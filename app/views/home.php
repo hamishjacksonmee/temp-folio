c87ec3ce3bd11910c97b7cb35c31f3776a00916b
Hamish Jackson-Mee <hamishjacksonmee@yahoo.co.nz>
2015-10-01 12:11:30 +1300

    new files

---
+++ app/views/home.php
@@ -0,0 +1,14 @@
+<div class="page-wrap">
+  <div class="intro"></div>
+  <div class="inner-wrap">
+    <p class="top-left"><strong>Hamish Jackson-Mee</strong></p>
+    <p class="mid">New Portfolio site is on its way.<br>
+    Grew tired of the old one. <a href="<?php echo PATH; ?>test">Test</a></p>
+    <p class="bottom-left"><strong>
+      <a href="mailto:contact@itshamish.com">contact@itshamish.com</a><br>
+      <a href="tel:+64210366352">+64 21 036 6352</a></strong>
+    </p>
+    <p class="bottom-right"><strong>Find me on</strong>&nbsp;&nbsp;&mdash;&nbsp;&nbsp;<a href="https://nz.linkedin.com/pub/hamish-jackson-mee/7b/b14/326" target="_blank">LinkedIn</a>
+    </p>
+  </div>
+</div>
---
+++ app/views/test.php
@@ -0,0 +1,17 @@
+<div class="effect-wrap">
+
+  <div class="outer-circle-wrap">
+    <div class="outer-circle">
+
+      <div class="outer-circle-image"></div>
+
+      <div class="inner-circle-wrap">
+        <div class="inner-circle">
+          <div class="inner-circle-image"></div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</div>
